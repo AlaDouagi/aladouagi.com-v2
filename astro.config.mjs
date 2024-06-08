a9cab7fd5a1fb24d5b5bfa0de69015c4424690b4
Alaeddine Douagi <ala.douagi@gmail.com>
2024-06-08 13:14:22 +0200

    Fix global site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://aladouagi-com-v2.vercel.app",
   publicDir: "./public",
   trailingSlash: "never",
   output: "static",
