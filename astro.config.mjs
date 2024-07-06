4c73c263357738d02563a573292202e48ff1d01b
Alaeddine Douagi <ala.douagi@gmail.com>
2024-07-06 15:12:27 +0200

    Integrate Tailwind with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
+import {defineConfig} from 'astro/config'
+import vercel from '@astrojs/vercel/static'
+
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aladouagi-com-v2.vercel.app",
-  publicDir: "./public",
-  trailingSlash: "never",
-  output: "static",
+  site: 'https://aladouagi-com-v2.vercel.app',
+  publicDir: './public',
+  trailingSlash: 'never',
+  output: 'static',
   adapter: vercel(),
-});
+  integrations: [tailwind()],
+})
