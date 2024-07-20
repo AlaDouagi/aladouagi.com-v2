6c9215dc665d64bb4051953c85863cc2e855b47a
Alaeddine Douagi <ala.douagi@gmail.com>
2024-07-20 14:11:10 +0200

    Reduce file size of OG images

--- src/pages/blog/[...slug]/og.png.ts
+++ src/pages/blog/[...slug]/og.png.ts
@@ -1,29 +1,29 @@
-import { getCollection, type CollectionEntry } from "astro:content";
-import fs from "node:fs";
-import path from "node:path";
-import { ImageResponse } from "@vercel/og";
+import {getCollection, type CollectionEntry} from 'astro:content'
+import fs from 'node:fs'
+import path from 'node:path'
+import {ImageResponse} from '@vercel/og'
 
 interface Props {
-  params: { slug: string };
-  props: { post: CollectionEntry<"blog"> };
-  request: Request;
+  params: {slug: string}
+  props: {post: CollectionEntry<'blog'>}
+  request: Request
 }
 
-const AUTHOR_NAME = "Alaeddine Douagi";
-const AUTHOR_JOB_TITLE = "Web Developer";
-const OG_BG_ROUTE = "/assets/og-bg-raw.jpg";
-const AUTHOR_PIC_ROUTE = "/assets/portrait-square.jpg";
+const AUTHOR_NAME = 'Alaeddine Douagi'
+const AUTHOR_JOB_TITLE = 'Web Developer'
+const OG_BG_ROUTE = '/assets/og-bg-raw.jpg'
+const AUTHOR_PIC_ROUTE = '/assets/portrait-square.jpg'
 
-export async function GET({ props, request }: Props) {
-  const { post } = props;
+export async function GET({props, request}: Props) {
+  const {post} = props
 
-  const OG_BG_URL = new URL(OG_BG_ROUTE, request.url).href;
-  const AUTHOR_PIC_URL = new URL(AUTHOR_PIC_ROUTE, request.url).href;
+  const OG_BG_URL = new URL(OG_BG_ROUTE, request.url).href
+  const AUTHOR_PIC_URL = new URL(AUTHOR_PIC_ROUTE, request.url).href
 
   // using custom font files
   const FontBold = fs.readFileSync(
-    path.resolve("./public/assets/fonts/kaisei-tokumin-bold.ttf")
-  );
+    path.resolve('./public/assets/fonts/kaisei-tokumin-bold.ttf'),
+  )
 
   // post cover with Image is pretty tricky for dev and build phase
   //   const postCover = fs.readFileSync(
@@ -36,91 +36,92 @@ export async function GET({ props, request }: Props) {
 
   // Astro doesn't support tsx endpoints so usign React-element objects
   const html = {
-    type: "div",
+    type: 'div',
     props: {
       style: {
-        position: "relative",
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start",
-        justifyContent: "center",
+        position: 'relative',
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
         backgroundImage: `url("${OG_BG_URL}")`,
+        backgroundSize: "960px 540px"
       },
       children: [
         {
-          type: "div",
+          type: 'div',
           props: {
             style: {
-              display: "flex",
-              fontSize: 130,
-              fontFamily: "Kaisei Tokumin",
-              letterSpacing: "-0.05em",
-              fontStyle: "normal",
-              color: "white",
-              lineHeight: "120px",
-              whiteSpace: "pre-wrap",
-              margin: "auto 190px",
+              display: 'flex',
+              fontSize: 65,
+              fontFamily: 'Kaisei Tokumin',
+              letterSpacing: '-0.05em',
+              fontStyle: 'normal',
+              color: 'white',
+              lineHeight: '60px',
+              whiteSpace: 'pre-wrap',
+              margin: 'auto 95px',
             },
             children: post.data.title,
           },
         },
         {
-          type: "div",
+          type: 'div',
           props: {
             style: {
-              postion: "absolute",
-              bottom: "3rem",
-              marginTop: "auto",
-              display: "flex",
-              width: "100%",
-              height: "190px",
-              padding: "0 190px",
+              postion: 'absolute',
+              bottom: '1.5rem',
+              marginTop: 'auto',
+              display: 'flex',
+              width: '100%',
+              height: '95px',
+              padding: '0 95px',
             },
             children: [
               {
-                type: "div",
+                type: 'div',
                 props: {
                   style: {
-                    position: "relative",
-                    height: "160px",
-                    width: "160px",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "flex-start",
-                    justifyContent: "center",
+                    position: 'relative',
+                    height: '80px',
+                    width: '80px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    justifyContent: 'center',
                     backgroundImage: `url("${AUTHOR_PIC_URL}")`,
-                    backgroundSize: "160px 160px",
-                    borderRadius: "100%",
+                    backgroundSize: '80px 80px',
+                    borderRadius: '100%',
                   },
                 },
               },
               {
-                type: "div",
+                type: 'div',
                 props: {
                   style: {
-                    display: "flex",
-                    flexDirection: "column",
-                    width: "100%",
-                    color: "white",
-                    paddingLeft: 32,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    width: '100%',
+                    color: 'white',
+                    paddingLeft: 16,
                   },
                   children: [
                     {
-                      type: "span",
+                      type: 'span',
                       props: {
                         style: {
-                          fontSize: 54,
+                          fontSize: 27,
                         },
                         children: AUTHOR_NAME,
                       },
                     },
                     {
-                      type: "span",
+                      type: 'span',
                       props: {
                         style: {
-                          fontSize: 36,
+                          fontSize: 18,
                         },
                         children: AUTHOR_JOB_TITLE,
                       },
@@ -133,26 +134,26 @@ export async function GET({ props, request }: Props) {
         },
       ],
     },
-  };
+  }
 
   return new ImageResponse(html, {
-    width: 1920,
-    height: 1080,
+    width: 960,
+    height: 540,
     fonts: [
       {
-        name: "Kaisei Tokumin",
+        name: 'Kaisei Tokumin',
         data: FontBold.buffer,
-        style: "normal",
+        style: 'normal',
       },
     ],
-  });
+  })
 }
 
 // to generate an image for each blog posts in a collection
 export async function getStaticPaths() {
-  const blogPosts = await getCollection("blog");
+  const blogPosts = await getCollection('blog')
   return blogPosts.map((post) => ({
-    params: { slug: post.slug },
-    props: { post },
-  }));
+    params: {slug: post.slug},
+    props: {post},
+  }))
 }
